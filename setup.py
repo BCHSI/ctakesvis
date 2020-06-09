55397b811a2e181ed102380554b6363739649549
Dmytro S Lituiev <d.lituiev@gmail.com>
2020-06-09 16:08:53 -0700

    data path fix

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=['pandas'],
-    package_data = {'static': 
-                    ['*.css', '*.js', 'tabulator/dist/js/tabulator.min.js']
+    package_data = {'conceptvis':
+                    ['static/*.css', 'static/*.js', 'static/tabulator/dist/js/tabulator.min.js']
                     },
     #packages=setuptools.find_packages(),
     # url="https://github.com/pypa/sampleproject",
