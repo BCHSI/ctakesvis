cab8a863a40c125426ef8e487db86e4879856e67
Dmytro S Lituiev <d.lituiev@gmail.com>
2020-06-09 15:33:46 -0700

    pip-ready

---
+++ setup.py
@@ -0,0 +1,29 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="conceptvis", # Replace with your own username
+    packages=['conceptvis',],
+    version="0.0.1",
+    author="Dima Lituiev",
+    author_email="d.lituiev@gmail.com",
+    description="package for concept (rich named entity) visualization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['pandas'],
+    package_data = {'static': 
+                    ['*.css', '*.js', 'tabulator/dist/js/tabulator.min.js']
+                    },
+    #packages=setuptools.find_packages(),
+    # url="https://github.com/pypa/sampleproject",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
